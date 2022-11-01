503855bc69230321c37c15c1432c31de92abf78e
smooth <68108963+smoothwastaken@users.noreply.github.com>
2022-11-01 11:26:21 +0100

    README little update + starting working on generate_map

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Lemmyngs
-NSI Project
+a cool I.T. school project.
+***🚧 README.md under construction... 🚧***
+
+## Documentation
+### *Block IDs and types*
+|IDs|Blocktypes|Location|
+|--|--|--|
+|0|`void`|`(0,0)`|
+|1|`ground`|`(48,16)`|
+|2|`brick`|`(48,0)`|
+|3|`-`|`-`|
+|4|`-`|`-`|
+|5|`-`|`-`|
+|6|`top_door`|`-`|
+|7|`bottom_door`|`-`|
+|8|`-`|`-`|
+|9|`-`|`-`|
+|10|`-`|`-`|
--- generate_map.py
+++ generate_map.py
@@ -9,27 +9,23 @@ import json
 def generateMap(fileName: str):
     try:
         file = open(f"./maps/{fileName}.kimarch", "x")
-        # print("Creating the file.")
+        print("[+] Creating the map file")
     except:
-        # print("File created !")
+        print("[*] Map already created")
         pass
     with open(f"./maps/{fileName}.kimarch", "w") as f:
+        # Creating an empty map
         theMap = []
-
-        startingDoor = [[0, 0], [1, 0]]
-
         for i in range(1, 21):
             line = []
             for j in range(1, 21):
-                if i == 3 or i == 4 or i == 7 or i == 8 or i == 11 or i == 12 or i == 16:
-                    line.append(2)
-                elif i == 15:
-                    line.append(1)
-                else:
-                    line.append(0)
+                line.append(0)
             theMap.append(line)
 
+        # Placing the starting door
+        startingDoor = [[0, 0], [1, 0]]
 
+        # Writing and closing the map file
         f.write(str(theMap))
         f.close()
 
