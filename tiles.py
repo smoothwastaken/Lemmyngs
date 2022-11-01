64366a64b8edfc21d7bbaa0aea5d2781d99054cf
smooth <68108963+smoothwastaken@users.noreply.github.com>
2022-11-01 11:51:43 +0100

    tiles: Adding new tile's IDs and types into Tile method.

--- tiles.py
+++ tiles.py
@@ -1,5 +1,6 @@
 from utils import printDocstring
 import pyxel
+import random
 
 class Tile(object):
     w = 16
@@ -8,17 +9,35 @@ class Tile(object):
     @classmethod
     def draw(cls, x, y, tileType):
         """Drawing a block on the screen."""
+        # Generating a random number for block variants
+        n = random.randint(0, 100)
+
         if tileType == "void":
-            pyxel.blt(x, y, 0, 0, 0, cls.w, cls.h)
+            pyxel.blt(x, y, 0, 240, 224, cls.w, cls.h)
 
         elif tileType == "ground":
-            pyxel.blt(x, y, 0, 48, 16, cls.w, cls.h)
+            if n % 2 == 0:
+                pyxel.blt(x, y, 0, 32, 16, cls.w, cls.h)
+            else:
+                pyxel.blt(x, y, 0, 48, 16, cls.w, cls.h)
 
         elif tileType == "brick":
-            pyxel.blt(x, y, 0, 48, 0, cls.w, cls.h)
+            if n % 2 == 0:
+                pyxel.blt(x, y, 0, 32, 0, cls.w, cls.h)
+            else:
+                pyxel.blt(x, y, 0, 48, 0, cls.w, cls.h)
 
-        elif tileType == "upDoor":
-            pass
+        elif tileType == "dirt":
+            pyxel.blt(x, y, 0, 0, 0, cls.w, cls.h)
 
-        elif tileType == "downDoor":
-            pass
+        elif tileType == "wooden_box":
+            pyxel.blt(x, y, 0, 16, 16, cls.w, cls.h)
+
+        elif tileType == "bomb":
+            pyxel.blt(x, y, 0, 16, 0, cls.w, cls.h)
+
+        elif tileType == "top_door":
+            pyxel.blt(x, y, 0, 0, 16, cls.w, cls.h)
+
+        elif tileType == "bottom_door":
+            pyxel.blt(x, y, 0, 0, 32, cls.w, cls.h)
